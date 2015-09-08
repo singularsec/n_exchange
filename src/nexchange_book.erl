2bd71e724c5539861cb6b12f121074cd271be6f6
hammett <hammett@gmail.com>
2015-09-08 17:57:41 -0300

    more tests on book creation

--- src/nexchange_book.erl
+++ src/nexchange_book.erl
@@ -1,7 +1,12 @@
 
 -module(nexchange_book).
 
+-ifdef(EUNIT).
 -compile(export_all).
+-else.
+-compile(export_all).
+% -export([]).
+-endif.
 
 -include("../include/secexchange.hrl").
 
@@ -14,7 +19,8 @@ create() ->
 % -record(order, {price, time, qtd, id}).
 insert_sell(#order{price = Price, time = _T, qtd = _Qtd, id = _Id} = Order,
             #book{sells = Sells} = Book) ->
-  {Key, Time} = order_key(Price),
+  NormalizedPrice = normalize_price(Price),
+  {Key, Time} = sell_order_key(NormalizedPrice),
   NewOrder = Order#order{oid = Key, time = Time},
   ets:insert(Sells, NewOrder),
   Book.
@@ -24,23 +30,48 @@ insert_sell(#order{price = Price, time = _T, qtd = _Qtd, id = _Id} = Order,
 
 insert_buy(#order{price = Price, time = _T, qtd = _Qtd, id = _Id} = Order,
             #book{buys = Buys} = Book) ->
-  {Key, Time} = order_key_inverted(Price),
+  NormalizedPrice = normalize_price(Price),
+  {Key, Time} = buy_order_key(NormalizedPrice),
   NewOrder = Order#order{oid = Key, time = Time},
   ets:insert(Buys, NewOrder),
   Book.
 
-dump_info(#book{sells = Sells, buys = Buys}) ->
-  lists:foldr (fun (E, Acc) -> Acc + " 1 " end, "", Sells).
+list_sells(#book{sells = Sells}, Collector, State) ->
+  traverse_ets_table(Sells, nil, Collector, State).
+
+list_buys(#book{buys = Buys}, Collector, State) ->
+  traverse_ets_table(Buys, nil, Collector, State).
+
+traverse_ets_table(Table, Key, Collector, State) ->
+  KeyToUse = case Key of
+    nil ->  ets:first(Table);
+    _ ->    ets:next(Table, Key)
+  end,
+  case KeyToUse of
+    '$end_of_table' ->
+      State;
+    _ ->
+      Order = ets:lookup(Table, KeyToUse),
+      NewState = Collector(Order, State),
+      traverse_ets_table(Table, KeyToUse, Collector, NewState)
+  end.
 
 % cancel, replace, change
+get_now() ->
+  erlang:system_time(micro_seconds). % - 1441736774862944.
 
-order_key(Price) ->
+sell_order_key(Price) ->
   % TODO: need to normalize number removing decimals
-  Now = erlang:system_time(micro_seconds),
+  Now = get_now(),
   Res = (Price * 100000000000000000) + Now,
   {Res, Now}.
 
-order_key_inverted(Price) ->
-  Now = erlang:system_time(micro_seconds) * -1,
-  Res = (Price * 100000000000000000) + Now,
+buy_order_key(Price) ->
+  % TODO: need to normalize number removing decimals
+  Now = get_now(),
+  Res = ((Price * -100000000000000000) + Now * 1),
   {Res, Now}.
+
+normalize_price(Price) ->
+  % remove 4 decimals by multipling by 10000
+  round(Price * 10000). % round converts it back to integer
