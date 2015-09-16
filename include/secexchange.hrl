9bfee1957a943224744a98c1bb11f32b3430e367
hammett <hammett@gmail.com>
2015-09-16 01:32:03 -0300

    added info to build exec report

--- include/secexchange.hrl
+++ include/secexchange.hrl
@@ -1,16 +1,21 @@
 
--record(order, {oid,
-                price,
-                time,
-                qtd,
+-record(order, {oid,  % used for sorting only!
+                symbol,
                 id,
-                ordertype,
-                limit=undefined,
+                qtd,
+                price,
+                price_type,
+                time,
+                order_type,
+                side,
+                stop_price,
                 timeinforce=day,
-                expiration=none,
+                expirationd=none,
+                expirationt=none,
                 account,
-                sessionid,
-                clOrdID,
+                from_sessionid,
+                to_sessionid,
+                cl_ord_id
                 }).
 
 -record(book,  {sells=[], buys=[], lasttrade=0}).
